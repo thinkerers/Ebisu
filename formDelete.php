8d55df5ecb0c1349314f2da673f046c104a8f88c
Avalario20 <luanosamsung@gmail.com>
2024-05-25 16:45:29 +0200

    add formDelete

---
+++ formDelete.php
@@ -0,0 +1,14 @@
+<?php
+/*if(!isset($_SESSION['auth'])){
+    header('Location: connexion.php');
+    exit;
+}*/
+?>
+<dialog>
+  <form method="dialog">
+  <p>Voulez-vous vraiment supprimer votre compte ?</p>
+  <button>Annuler</button>
+  <button>Supprimer</button>
+  </form>
+</dialog>
+<button onclick="document.querySelector('dialog').showModal();">Supprimer le compte</button>
