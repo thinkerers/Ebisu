22457fd4b380cc9a31d90b33403249bdec72b03c
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-05-28 01:41:43 +0200

    on devrait éviter les variables globales

--- model/model.php
+++ model/model.php
@@ -1,6 +1,6 @@
 <?php
 function deleteUser($idUser){
-    global $bdd;
+    global $bdd; //bad practice: https://www.reddit.com/r/PHP/comments/2lct7y/question_do_you_use_globals_or_not/
     $deleteUser = $bdd->prepare('DELETE FROM user WHERE id = :id');
     $deleteUser->bindParam(':id', $idUser);
     $deleteUser->execute();
