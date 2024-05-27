64867989b2dad7c8fb106930ecfcac8ba8f596e9
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-05-28 01:36:31 +0200

    change la méthode de connexion (sqlite au lieu de mysql)

--- model/dbConnect.php
+++ model/dbConnect.php
@@ -1,10 +1,27 @@
 <?php
-try{
-    $dbname = 'ebisu db';//Nom de la base de donnée
-    $bdd = new PDO('mysql:host=localhost;dbname='.$dbname.';charset=utf8','root','');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    global $bdd;
-}catch (Exception $e){
-    die("Une erreur a été trouvé :.$e->getMessage()");
+class dbConnect extends SQLite3 {
+    function __construct() {
+        try {
+            $this->open('ebisu.sqlite');
+        } catch (Exception $e) {
+            // Log the error to a file (replace 'error_log.txt' with your desired path)
+            error_log(
+                date('[Y-m-d H:i:s]') . 
+                " EbisuDB Error: " . $e->getMessage() . PHP_EOL,
+                3, 'error_log.txt'
+            ); 
+            die("Error opening database. Please try again later."); 
+        }
+    }
 }
+
+// $db = new dbConnect();
+
+// $db->exec('CREATE TABLE foo (bar STRING)');
+// $db->exec("INSERT INTO foo (bar) VALUES ('This is a test')");
+
+// $result = $db->query('SELECT bar FROM foo');
+// var_dump($result->fetchArray());
+
+
 ?>
