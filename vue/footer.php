0da100ea9c445db654e6b0bbdc9ffb376a492d8a
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-05-28 03:13:16 +0200

    Ajout d'un footer avec lien temporaire vers adminer pour faciliter le debug

--- vue/footer.php
+++ vue/footer.php
@@ -1,2 +1,6 @@
+<footer>
+    <a href="http://ebisu.test/model/adminer/sqlite.php?sqlite=&username=&db=..%2Febisu.sqlite&select=users">adminer</a>
+    <p>&copy; 2024 - Thinkerers</p>
+</footer>
 </body>
 </html>
