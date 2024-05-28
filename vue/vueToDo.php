52c8a431533954cc6f30f6289ea8bf1f324470e7
LeaOlala <145347828+LeaOlala@users.noreply.github.com>
2024-05-28 20:06:44 +0200

    nouveau to-do form

--- vue/vueToDo.php
+++ vue/vueToDo.php
@@ -1,9 +1,24 @@
 <?php include("head.php"); ?>
 
-<h1>To-Do List</h1>
+    <h1>To-Do List</h1>
+    
+    <!-- formulaire de nouvelle tâche -->
+    <form action="?" method="post" class="toDoForm">
+        <small>Titre de la tâche</small></br>
+        <input type="text" name="toDoTitleInput" required></br>
 
-<!-- formulaire de nouvelle tâche -->
+        <small>Description de la tâche</small></br>
+        <textarea name="toDoDescriptionInput" required></textarea></br>
 
-<!-- affichage des tâches -->
+        <button type="button" class="removeSectionBtn" title="Supprimer la section"></button>
+
+        <button type="button" id="addSectionBtn" title="Ajouter une section"></button>
+
+
+        <input type="submit" name="toDoSubmit"vvalue="valider"> 
+    </form>
+    
+    <!-- affichage des tâches -->
+    <h2>Tâches à réaliser :</h2>
 
 <?php include("footer.php") ?>
