a7eae7b5affe079beed363164c14380f5e1ce72c
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-05-28 03:36:09 +0200

    ajouts de liens vers github dans le footer

--- vue/footer.php
+++ vue/footer.php
@@ -1,5 +1,8 @@
 <footer>
+    <a href="/">accueil</a>
     <a href="http://ebisu.test/model/adminer/sqlite.php?sqlite=&username=&db=..%2Febisu.sqlite&select=users">adminer</a>
+    <a href="https://github.com/thinkerers/Ebisu">github</a>
+    <a href="https://github.com/thinkerers/Ebisu/wiki">wiki</a>
     <p>&copy; 2024 - Thinkerers</p>
 </footer>
 </body>
