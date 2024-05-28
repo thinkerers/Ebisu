ce0adf0556838d2d8d18d007d7cc71bc4a0436ad
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-05-28 05:40:29 +0200

    Ajout d'icones dans le footer

--- vue/footer.php
+++ vue/footer.php
@@ -1,10 +1,10 @@
 </main>
-<footer>
-    <a href="/">accueil</a>
-    <a href="http://ebisu.test/model/adminer/sqlite.php?sqlite=&username=&db=..%2Febisu.sqlite&select=users">adminer</a>
-    <a href="https://github.com/thinkerers/Ebisu">github</a>
-    <a href="https://github.com/thinkerers/Ebisu/wiki">wiki</a>
-    <p>&copy; 2024 - Thinkerers</p>
+<footer class="material-symbols-outlined" >
+    <a title="Accueil" href="/">home</a>
+    <a title="Ouvrir adminer" href="http://ebisu.test/model/adminer/sqlite.php?sqlite=&username=&db=..%2Febisu.sqlite&select=users" >admin_panel_settings</a>
+    <a title="github" href="https://github.com/thinkerers/Ebisu">folder_data</a>
+    <a title="wiki" href="https://github.com/thinkerers/Ebisu/wiki">unknown_document</a>
+    <p>🐠 2024 - Thinkerers</p>
 </footer>
 </body>
 </html>
