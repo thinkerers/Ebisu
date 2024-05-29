0f4e6008e313fc7904c70f483cb05dddec01cbc3
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-05-29 16:54:00 +0200

    change la vue de création pour utiliser le pattern MVC (voir https://openclassrooms.com/fr/courses/4670706-adoptez-une-architecture-mvc-en-php/7848103-creez-un-template-de-page)

--- vue/layout.php
+++ vue/layout.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
-
 <head>
     <meta charset="utf-8" />
     <title><?= $title ?></title>
-    <link href="style.css" rel="stylesheet" />
+    <link href="vue/style.css" rel="stylesheet" />
 </head>
 
 <body>
     <?= $content ?>
+    <footer class="material-symbols-outlined">
+        <a title="Accueil" href="/">home</a>
+        <a title="Ouvrir adminer" href="http://ebisu.test/model/adminer/sqlite.php?sqlite=&username=&db=..%2Febisu.sqlite&select=users">admin_panel_settings</a>
+        <a title="github" href="https://github.com/thinkerers/Ebisu">folder_data</a>
+        <a title="wiki" href="https://github.com/thinkerers/Ebisu/wiki">unknown_document</a>
+        <p>🐠 2024 - Thinkerers</p>
+    </footer>
 </body>
 
 </html>
