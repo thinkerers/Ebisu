18d15088f8f89a74415a505299e4ef7c2e70a067
LeaOlala <145347828+LeaOlala@users.noreply.github.com>
2024-05-31 20:07:00 +0200

    form data by $_post

--- vue/vueToDo.php
+++ vue/vueToDo.php
@@ -1,31 +1,62 @@
-<?php include("head.php"); ?>
+<?php
 
-<h1>To-Do List</h1>
+    // Form answear
+    if(isset($_POST["taskSubmit"])){
 
-<!-- formulaire de nouvelle tâche -->
-<form action="self" method="post" class="taskForm">
-    
-    <fieldset id="taskContainer">
-        <ul >
-            <li>
-                <small>Titre de la tâche</small></br>
-                <input type="text" name="taskTitleInput" required></br>
+        //  form data
+        $taskTitleInput = $_POST["taskTitleInput"];
+        $taskDescriptionInput = $_POST["taskDescriptionInput"];
+        $taskSubmit = $_POST["taskSubmit"];
+
+        // Send data to database
+        foreach($taskTitleInput as $taskTitle){
+            // connect db
+            // new entry in table
+            // prepare
+            // execute
+        }
+
+        // display tasks
+            // connect db
+            // Select from table
+                // prepare
+                // fetch
             
-                <small>Description de la tâche</small></br>
-                <textarea name="taskDescriptionInput" required></textarea></br>
+            // loop each fetch
+                // echo
+
+    }
+
+    // Form
+    else{ include("head.php"); ?>
+
+        <h1>To-Do List</h1>
+        <!-- new task form -->
+        <form action="vueToDo.php" method="post" class="taskForm">
+            
+            <fieldset id="taskContainer">
+                <ul >
+                    <li>
+                        <small>Titre de la tâche</small></br>
+                        <input type="text" name="taskTitleInput" required></br>
+                    
+                        <small>Description de la tâche</small></br>
+                        <textarea name="taskDescriptionInput" required></textarea></br>
+                        
+                        <button type="button" class="removeTaskBtn" title="Supprimer la tâche">Supprimer</button>
+                    </li>
+                </ul>
                 
-                <button type="button" class="removeTaskBtn" title="Supprimer la tâche">Supprimer</button>
-            </li>
-        </ul>
-        
-        <button type="button" id="addTaskBtn" title="Ajouter une tache">+</button>
-    </fieldset>
-    
-    <input type="submit" name="taskSubmit" value="valider"> 
-</form>
+                <button type="button" id="addTaskBtn" title="Ajouter une tache">+</button>
+            </fieldset>
+            
+            <input type="submit" name="taskSubmit" value="valider"> 
+        </form>
+        <?php 
+    } 
 
-<!-- affichage des tâches -->
-<h2>Tâches à réaliser :</h2>
+// footer
+include("footer.php") ?> 
 
-<?php include("footer.php") ?>
+<!-- srcipt -->
 <script src="toDo.js"></script>
