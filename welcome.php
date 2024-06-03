dd9cb1dc4f7d168bbc8c281b5b58cc0fc3845f43
LeaOlala <145347828+LeaOlala@users.noreply.github.com>
2024-06-03 19:08:56 +0200

    ajout de vue to do

--- welcome.php
+++ welcome.php
@@ -1,5 +1,7 @@
 <?php
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
+}
 
 if (!isset($_SESSION['user'])) {
     header('Location: login.php'); // Redirect to login if not authenticated
@@ -13,4 +15,5 @@ include_once './vue/head.php';
 <h1>Bienvenue sur Ebisu !</h1>
 <p><a href="vue/formDelete.php">Supprimer mon compte</a></p>
 
-<?php include_once './vue/footer.php'; ?>
+<?php include_once 'vue/vueToDo.php';
+include_once './vue/footer.php'; ?>
