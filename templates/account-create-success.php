df933d822dab015129afb87572ab940a6da84260
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-06-06 04:35:40 +0200

    remove useless files and move the success message to the templates

--- templates/account-create-success.php
+++ templates/account-create-success.php
@@ -4,7 +4,7 @@ ob_start();
 ?>
 <h1>Account Created Successfully</h1>
 <p>Thank you for creating an account. Your account has been successfully created.</p>
-<p>You can now <a href="login.php">log in</a> to your account.</p>
+<p>You can now <a href="/?action=login">log in</a> to your account.</p>
 <?php
 $content = ob_get_clean();
 require($_SERVER['DOCUMENT_ROOT'].'/templates/layout.php');
