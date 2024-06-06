81d16732ce76e6343e39dec238f3253aa8863cb0
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-06-06 10:46:46 +0200

    remove root on render function

--- src/controllers/Page.php
+++ src/controllers/Page.php
@@ -5,9 +5,9 @@ class Page
     public function render()
     {
         if (isset($_SESSION['user'])) {
-            require($_SERVER['DOCUMENT_ROOT'].'/templates/welcome.php');
+            require('templates/welcome.php');
         } else {
-            require($_SERVER['DOCUMENT_ROOT'].'/templates/account-form-login.php');
+            require('templates/account-form-login.php');
         }
     }
 }
