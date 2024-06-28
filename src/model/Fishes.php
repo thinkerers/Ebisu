9aec8a7182730d080a87c645b9777e8a871dcea0
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-06-29 01:14:02 +0200

    move the method related to fish to the fishes class

--- src/model/Fishes.php
+++ src/model/Fishes.php
@@ -1,6 +1,24 @@
 <?php
 
 namespace src\model;
+/* ## Entity-Relationship Diagram
+
+    +-------------+       +--------------+
+    |  compendium |       |    fishes    |
+    +-------------+       +--------------+
+    | id          |<----->| compendiumId |
+    | rank        |       | userId       |
+    | name        |       | caughtTime   |
+    | description |       | id           |
+    | img         |       +--------------+
+    +-------------+
+
+    ## Description
+
+    The `compendium` table stores each type of fishes available. Each type of fish has a unique ID, a rank, a name, a description, and an image URL.
+
+    The `fishes` table stores the fish caught by each user. Each fishe has a unique ID, a user ID, a fish ID, and a timestamp.
+*/
 
 /**
  * Class Fishes
@@ -20,13 +38,50 @@ class Fishes
      * @param int $id
      * @param int $userId
      * @param int $fishId
-     * @param string $caughtTime
+     * @param string|null $caughtTime
+     * @param dbConnect|null $db
      */
     public function __construct(
         public int $id, 
         public int $userId, 
         public int $fishId, 
-        public ?string $caughtTime
+        public ?string $caughtTime,
+        private ?dbConnect $db
         )
     {}
+
+    public function getDiscovered($userId)
+    {
+        try {
+            $statement = $this->db->prepare('SELECT fishId FROM fishes WHERE userId = :userId');
+            $statement->bindValue(':userId', $userId);
+            $result = $statement->execute();
+            $discoveredFish = [];
+            while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
+                $discoveredFish[] = $row['fishId'];
+            }
+            return $discoveredFish;
+        } catch (\Exception $e) {
+            error_log("Fish retrieval error: " . $e->getMessage());
+            return [];
+        }
+    }
+
+    /**
+     * Stores the fish data in the database.
+     *
+     * @param object $fish The fish data to store.
+     * @param int $userId The ID of the user who caught the fish.
+     */
+    public function storeFish($fish, $userId)
+    {
+        try {
+            $statement = $this->db->prepare('INSERT INTO fishes (fishId, userId) VALUES (:fishId, :userId)');
+            $statement->bindValue(':fishId', $fish->fishId);
+            $statement->bindValue(':userId', $userId);
+            $statement->execute();
+        } catch (\Exception $e) {
+            error_log("Fish storage error: " . $e->getMessage());
+        }
+    }
 }
