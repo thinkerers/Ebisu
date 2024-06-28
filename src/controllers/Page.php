2e35fd042f8373630ec93f56e5eed65ca2f653c3
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-06-28 21:14:24 +0200

    handle the fishing page in the Page class

--- src/controllers/Page.php
+++ src/controllers/Page.php
@@ -2,6 +2,11 @@
 namespace src\controllers;
 class Page
 {
+    public function __construct(
+        private $fishing = new \src\lib\Fishing()
+    )
+    {}
+
     public function render()
     {
         if (isset($_SESSION['user'])) {
@@ -22,4 +27,29 @@ class Page
             $_SESSION['start'] = $time;
         }
     }
+
+    public function goFishing()
+    {
+
+        // initialize the discoveredFishes session variable if it does not exist
+        if (!isset($_SESSION['discoveredFishes'])) {
+            // get the discovered fishes from the database
+            $_SESSION['discoveredFishes'] = $this->fishing->discovered($_SESSION['user']['id']);
+        }
+
+        $fish = $this->fishing->catch();
+        $this->fishing->save($fish,$_SESSION['user']['id']);
+
+        //update the discovered fish in the session
+        $_SESSION['discoveredFishes'][$fish->fishId] = ($_SESSION['discoveredFishes'][$fish->fishId] ?? 0) + 1;
+
+         // Prepare data for the template
+        $data = [
+            'fish' => $fish,
+            'discoveredFishes' => $_SESSION['discoveredFishes']
+        ];
+
+            require 'templates/fishing.php';
+        } 
+
 }
