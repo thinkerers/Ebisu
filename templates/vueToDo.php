9856fdc9c3a24fd63f6e70f8a3163e23b09083e7
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-06-29 06:44:30 +0200

    fix tasks

--- templates/vueToDo.php
+++ templates/vueToDo.php
@@ -3,57 +3,26 @@ $title = 'Créer un compte';
 $style ='@import url(public/css/form.css);';
 ob_start(); 
 ?>
-<div class="book">
-    <form method="post" class="taskDisplay">
-        <legend><h2>To-do list</h2></legend>
-        <?php
-foreach ($_SESSION["tasks"] as $id => $name) {
-    echo '
-        <label class="task">
-            <input type="checkbox" name="task[]" value="' . $id . '" />
-            ' . htmlspecialchars($name) . '
-            <button class="delete" type="submit" name="removeTask" value="' . $id . '" title="Supprimer la tâche">X</button>
-        </label>
-    ';
-}
-?>
-    </form>
 
-    <style>
-        .book{
-            overflow-y: scroll;
-        }
-        .task{
-            display: grid;
-            grid-auto-flow: column;
-            align-content: center;
-        }
-        [type="checkbox"]{
-            height: 2lh;
-            aspect-ratio: 1;
-            margin:0;
-        }
-    </style>
-    
-    <!-- formulaire de nouvelle tâche -->
-    <form method="post" class="taskForm">
-        <fieldset id="taskContainer">
-            <ul class="createToDo">
-                <li>
-                    <small>Titre de la tâche</small></br>
-                    <input type="text" name="taskTitle" required></br>
-                
-                    <!-- <small>Description de la tâche</small></br>
-                    <textarea name="taskDescription" required></textarea></br> -->
-                </li>
-            </ul>
-            
-            <button type="submit" name="addTask" title="Ajouter une tache">ajouter</button>
-        </fieldset>
-        
-        <!-- <input type="submit" name="taskSubmit" value="valider">  -->
-    </form>
-</div>
+<form class="book taskDisplay" method="post">
+    <legend><h2>To-do list</h2></legend>
+    <label>
+        <small>Nouvelle tâche</small><br>
+        <input autofocus type="text" name="taskTitle" minlength="3">
+        <button tabindex="0"  type="submit" name="addTask" title="Ajouter une tâche">ajouter</button>
+    </label>
+
+    <?php foreach ($_SESSION["tasks"] as $id => $name) : ?>
+        <label class="task">
+            <input type="checkbox" name="task[]" value="<?= htmlspecialchars($id) ?>" />
+            <?= htmlspecialchars($name) ?>
+            <button tabindex="1" class="delete" type="submit" name="removeTask" value="<?= htmlspecialchars($id) ?>" title="Supprimer la tâche">X</button>
+        </label>
+        <br>
+    <?php endforeach; ?>
+</form>
+
+
 
 
 <?php
