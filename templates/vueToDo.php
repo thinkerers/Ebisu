9f14e9d2f7acf3aef01976d2ecd49941dd1c0564
Théophile <34781168+DesignThinkerer@users.noreply.github.com>
2024-06-29 07:30:38 +0200

    improve task layout

--- templates/vueToDo.php
+++ templates/vueToDo.php
@@ -6,19 +6,16 @@ ob_start();
 
 <form class="book taskDisplay" method="post">
     <legend><h2><?= $title ?></h2></legend>
-    <label>
-        <small>Nouvelle tâche</small><br>
+    <label class="addTask">
         <input autofocus type="text" name="taskTitle" minlength="3">
-        <button tabindex="0"  type="submit" name="addTask" title="Ajouter une tâche">ajouter</button>
+        <button tabindex="0"  type="submit" name="addTask" title="Ajouter une tâche">+</button>
     </label>
 
     <?php foreach ($data['tasks'] as $id => $name) : ?>
         <label class="task">
-            <input type="checkbox" name="task[]" value="<?= htmlspecialchars($id) ?>" />
             <?= htmlspecialchars($name) ?>
             <button tabindex="1" class="delete" type="submit" name="removeTask" value="<?= htmlspecialchars($id) ?>" title="Supprimer la tâche">X</button>
         </label>
-        <br>
     <?php endforeach; ?>
 </form>
 <?php
